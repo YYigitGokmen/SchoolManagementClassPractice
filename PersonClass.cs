4f600b2f3bbd87c6cff9988920e18e775b90ecf9
Yılmaz Yiğit Gökmen <gokmenyyigit@gmail.com>
2024-07-30 10:57:02 +0300

    Add files via upload

---
+++ PersonClass.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchoolManagementPractice
+{
+    public class PersonClass
+    {
+            //In order to encapsulate. These features should be in private 
+            private string name;
+            private string surName;
+            private int dateOfBirth;
+         
+            //In order to reach properties. I declared them as public.
+            public string Name
+            {
+                get
+                {
+                    return name;
+
+                }
+                set
+                {
+                    name = value;
+                }
+
+            }
+
+            public string Surname
+            {
+                get
+                {
+                    return surName;
+                }
+
+                set
+                {
+                     surName = value;
+                    
+                }
+            }
+
+            public int DateOfBirth
+            {
+                get
+                {
+                return dateOfBirth;
+                }
+                set
+                {
+                    if (value >= 1960)
+                    {
+                    dateOfBirth = value;
+                    }
+                }
+
+            }
+
+            public void showStudent()
+            {
+            Console.WriteLine($"Öğrencinin adı:  {name}\nSoyadı :  {surName}\nDoğumTarihi : {DateOfBirth}");
+        }
+
+
+
+
+        public void showTeacher()
+        {
+            Console.WriteLine($"Öğretmenin adı:  {name}\n Soyadı :  {surName}\nDoğumTarihi : {DateOfBirth}");
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿
+using SchoolManagementPractice;
+
+
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        //I created few student and teacher objects from
+        //PersonClass and i print their informations via using methods
+
+        PersonClass StudentOne = new PersonClass();
+
+        {
+            StudentOne.Name = "Yiğit";
+            StudentOne.Surname = "Gökmen";
+            StudentOne.DateOfBirth = 1998;
+
+            StudentOne.showStudent();
+
+        }
+
+        Console.WriteLine("");
+
+        PersonClass StudentTwo = new PersonClass();
+
+        {
+            StudentTwo.Name = "Ali";
+            StudentTwo.Surname = "Veli";
+            StudentTwo.DateOfBirth = 2000;
+
+            StudentTwo.showStudent();
+
+        }
+
+        Console.WriteLine("");
+
+        PersonClass TeacherOne = new PersonClass();
+        {
+
+            TeacherOne.Name = "Öğretmen Hasan";
+            TeacherOne.Surname = "Ali";
+            TeacherOne.DateOfBirth = 1980;
+
+            TeacherOne.showTeacher();
+        }
+
+
+
+
+
+
+
+
+
+    }
+
+}
+
+
+
+
